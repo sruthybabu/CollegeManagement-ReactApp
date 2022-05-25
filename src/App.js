347083ba0cb6fe5ce3ba4f1908e69f71690b79b6
sruthybabu <sruthybabu64@gmail.com>
2022-05-26 01:09:16 +0530

    search student complete

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import AddStudent from './components/AddStudent';
 import AddFaculty from './components/AddFaculty';
+import SearchStudent from './components/SearchStudent';
 
 function App() {
   return (
     <div>
 <AddStudent/>
 <AddFaculty/>
+<SearchStudent/>
     </div>
   );
 }
